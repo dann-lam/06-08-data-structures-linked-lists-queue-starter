f1073c57931b9a6dcfcdf756a722263c2df2a5c4
Daniel Lam <Dann.lam@gmail.com>
2022-06-08 15:23:14 -0700

    Committed

--- implementation/02-doubly-linked-list.js
+++ implementation/02-doubly-linked-list.js
@@ -6,7 +6,9 @@ class DoublyLinkedNode {
         this.prev = null;
     }
 }
-
+// let kirby = new DoublyLinkedList
+// kirby.addToHead(9)
+// console.log(kirby)
 class DoublyLinkedList {
     constructor() {
         this.head = null;
@@ -21,48 +23,85 @@ class DoublyLinkedList {
         // Add node of val to head of linked list
         let newNode = new DoublyLinkedNode(val);
 
-        if (this.length >= 0) {
-            this.head.previous = newNode;
-            newNode.next = this.head;
+        if (!this.head) {
+            this.tail = newNode;
             this.head = newNode;
         } else {
-            this.head = newNode;
-            this.tail = newNode;
+            newNode.next = this.head;
+
+            this.head.prev = newNode;
+
+            this.head = newNode
         }
 
         this.length++;
     }
 
     addToTail(val) {
+        let newNode = new DoublyLinkedNode(val)
+        if(!this.head){
+            this.head = newNode
+            this.tail = newNode
+        } else {
+            this.tail.next = newNode
+            newNode.prev = this.tail
+            this.tail = newNode
+        }
+        // if (this.length >= 1) {
+        //     this.tail.next = newNode;
+        //     newNode.prev = this.tail;
+        //     this.tail = newNode;
+        //   } else {
+        //     this.head = newNode;
+        //     this.tail = newNode;
+        //   }
+
+          this.length += 1;
+        }
         // Add node of val to tail of linked list
 
         // Write your hypothesis on the time complexity of this method here
-    }
+
 
     removeFromHead() {
-        // Remove node at head
-
+        if(!this.head) return undefined
+        const oldHead = this.head
+        this.head = oldHead.next
+        if(this.head){
+        this.head.prev = null
+        }
+        this.length--
+        return oldHead.value
         // Write your hypothesis on the time complexity of this method here
     }
 
     removeFromTail() {
-        // Remove node at tail
-
-        // Write your hypothesis on the time complexity of this method here
+        if(!this.head) return undefined
+        const oldTail = this.tail
+        this.tail = oldTail.prev
+        if(this.tail){
+            this.tail.next = null
+        }
+        this.length--
+        return oldTail.value
     }
 
     peekAtHead() {
-        // Return value of head node
-
+        if(!this.head) return;
+        return this.head.value;
         // Write your hypothesis on the time complexity of this method here
     }
 
     peekAtTail() {
-        // Return value of tail node
-        
+       if(!this.head) return;
+       return this.tail.value;
         // Write your hypothesis on the time complexity of this method here
     }
 }
+
+
+// kirby.addToHead('lol')
+// console.log(kirby)
 
 module.exports = {
     DoublyLinkedList,
